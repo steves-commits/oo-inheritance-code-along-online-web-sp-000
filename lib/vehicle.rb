310a24368932aefcd4f2b5c28f46e21551bcf100
Steven Topazio <topazio.steven@gmail.com>
2020-11-20 18:59:00 +0000

    Automatically backed up by Learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,7 +1,9 @@
 class Vehicle
   
   attr_accessor :wheel_size, :wheel_number
-  @wheel_size = wheel_size
+ 
+ def initialize(wheel_size, wheel_number)
+ @wheel_size = wheel_size
   @wheel_number = wheel_number 
 end
 
